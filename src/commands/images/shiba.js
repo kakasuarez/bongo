bb9b7ac1897e66c6a7e5934b78f9e87b842580be
Vatsal Veerwal <vatsalveerwal@gmail.com>
2021-02-02 12:14:06 +0530

    Added `shiba` command.

---
+++ src/commands/images/shiba.js
@@ -0,0 +1,33 @@
+const { Command } = require("discord.js-commando");
+const fetch = require("node-fetch");
+
+class ShibaCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: "shiba",
+			group: "images",
+			memberName: "shiba",
+			description: "Returns random images of Shiba Inu dogs.",
+			args: [
+				{
+					key: "count",
+					prompt: "How many images do you want?",
+					type: "string",
+					default: "1",
+				},
+			],
+		});
+	}
+
+	run(message, { count }) {
+		fetch(`http://shibe.online/api/shibes?count=${count}`)
+			.then((res) => res.json())
+			.then((json) => {
+				json.forEach((image) => {
+					message.say(image);
+				});
+			});
+	}
+}
+
+module.exports = ShibaCommand;
